cd0cbbaedfb6a1d9eae682761bd0beccbccdc7a6
Sandeep Raghuraman <sandy.8925@gmail.com>
2018-10-31 03:34:04 +0530

    Add some more board configs

--- lenovo/yt2/BoardConfig.mk
+++ lenovo/yt2/BoardConfig.mk
@@ -1,1 +1,8 @@
 include build/target/board/generic_x86_64_a/BoardConfig.mk
+
+TARGET_NO_KERNEL := false
+TARGET_NO_RECOVERY := false
+BOARD_USES_RECOVERY_AS_BOOT := false
+BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
+
+BOARD_AVB_ENABLE := false
