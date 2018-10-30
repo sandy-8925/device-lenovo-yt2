db5be97ce344d9994713c3f3709792878e8ae47a
Sandeep Raghuraman <sandy.8925@gmail.com>
2018-10-30 20:44:46 +0530

    Start creating basic Lenovo YT2 makefiles

---
+++ lenovo/yt2/aosp_yt2.mk
@@ -0,0 +1,10 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_x86_64_a.mk)
+
+PRODUCT_NAME := aosp_yt2
+PRODUCT_DEVICE := yt2
+PRODUCT_BRAND := Android
+PRODUCT_MODEL := Yoga Tab 2
+PRODUCT_MANUFACTURER := Lenovo
+
+
