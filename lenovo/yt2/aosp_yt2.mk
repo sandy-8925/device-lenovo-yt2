e8c356e4c763f25f1d728d8661dfc91e08f04d5e
Sandeep Raghuraman <sandy.8925@gmail.com>
2018-11-05 16:42:35 +0530

    Rearrange some configs, change firmware path to /vendor/firmware and make sure the kernel is told the firmware path as well

--- lenovo/yt2/aosp_yt2.mk
+++ lenovo/yt2/aosp_yt2.mk
@@ -3,8 +3,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_x86_64_a.mk)
 
 PRODUCT_COPY_FILES := device/lenovo/yt2/config_files/fstab.hardware:system/etc/fstab.yt2
 
-$(call inherit-product-if-exists, vendor/lenovo/yt2/vendor-blobs.mk)
-
 PRODUCT_NAME := aosp_yt2
 PRODUCT_DEVICE := yt2
 PRODUCT_BRAND := Android
@@ -14,3 +12,7 @@ PRODUCT_MANUFACTURER := Lenovo
 PRODUCT_CHARACTERISTICS := tablet
 PRODUCT_AAPT_CONFIG := normal
 BUILD_ARM_FOR_X86 := true
+
+TARGET_COPY_OUT_VENDOR := vendor
+FIRMWARE_DIR_NAME := firmware
+$(call inherit-product-if-exists, vendor/lenovo/yt2/vendor-blobs.mk)
