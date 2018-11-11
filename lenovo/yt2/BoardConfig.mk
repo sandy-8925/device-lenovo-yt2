116f49a8684b6c438f1c8c86a9ba3bb60f1e8155
Sandeep Raghuraman <sandy.8925@gmail.com>
2018-11-12 04:40:29 +0530

    First working version that boots kernel!
    - Don't do system as root - device's bootloader expects a ramdisk to be
    present in the boot.img, and won't boot successfully if there's no
    ramdisk
    - Set some mkbootimg args

--- lenovo/yt2/BoardConfig.mk
+++ lenovo/yt2/BoardConfig.mk
@@ -3,7 +3,7 @@ include build/target/board/generic_x86_64_a/BoardConfig.mk
 TARGET_NO_KERNEL := false
 TARGET_NO_RECOVERY := true
 BOARD_USES_RECOVERY_AS_BOOT := false
-BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
+BOARD_BUILD_SYSTEM_ROOT_IMAGE := false
 
 BOARD_AVB_ENABLE := false
 
@@ -24,13 +24,14 @@ BOARD_CACHEIMAGE_PARTITION_SIZE := 134217728
 
 BOARD_FLASH_BLOCK_SIZE := 2048
 
-BOARD_KERNEL_CMDLINE += root=/dev/block/mmcblk0p13
 BOARD_KERNEL_CMDLINE += printk.devkmsg=on earlyprintk=vga loglevel=8
 BOARD_KERNEL_CMDLINE += androidboot.hardware=yt2 androidboot.spid=xxxx:xxxx:xxxx:xxxx:xxxx:xxxx androidboot.serialno=01234567890123456789 androidboot.selinux=permissive
 BOARD_KERNEL_CMDLINE += firmware_class.path=/vendor/firmware
 BOARD_KERNEL_CMDLINE += vga=current i915.modeset=1 drm.vblankoffdelay=1 acpi_backlight=vendor i915.mipi_panel_id=3
 
 BOARD_BOOT_HEADER_VERSION := 0
-BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
+BOARD_PAGESIZE := 2048
+OS_VERSION := 9
+BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION) --pagesize $(BOARD_PAGESIZE) --os_version $(OS_VERSION)
 
 TARGET_PREBUILT_KERNEL := $(TARGET_KERNEL_SOURCE)/arch/$(TARGET_KERNEL_ARCH)/boot/$(BOARD_KERNEL_IMAGE_NAME)
