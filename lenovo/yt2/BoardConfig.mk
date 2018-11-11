2775196b1ba12ee6745e7e3bea3aa5f99f2800da
Sandeep Raghuraman <sandy.8925@gmail.com>
2018-11-12 01:56:59 +0530

    Enable early boot messages - https://www.linuxtopia.org/online_books/linux_kernel/kernel_configuration/re05.html

--- lenovo/yt2/BoardConfig.mk
+++ lenovo/yt2/BoardConfig.mk
@@ -25,7 +25,7 @@ BOARD_CACHEIMAGE_PARTITION_SIZE := 134217728
 BOARD_FLASH_BLOCK_SIZE := 2048
 
 BOARD_KERNEL_CMDLINE += root=/dev/block/mmcblk0p13
-BOARD_KERNEL_CMDLINE += printk.devkmsg=on loglevel=8
+BOARD_KERNEL_CMDLINE += printk.devkmsg=on earlyprintk=vga loglevel=8
 BOARD_KERNEL_CMDLINE += androidboot.hardware=yt2 androidboot.spid=xxxx:xxxx:xxxx:xxxx:xxxx:xxxx androidboot.serialno=01234567890123456789 androidboot.selinux=permissive
 BOARD_KERNEL_CMDLINE += firmware_class.path=/vendor/firmware
 BOARD_KERNEL_CMDLINE += vga=current i915.modeset=1 drm.vblankoffdelay=1 acpi_backlight=vendor i915.mipi_panel_id=3
