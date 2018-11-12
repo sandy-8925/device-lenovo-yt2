ce34002fd1e10e48a51b049c0b9f2c2ff95e29cd
Sandeep Raghuraman <sandy.8925@gmail.com>
2018-11-12 21:29:53 +0530

    Added some comments and documentations to BoardConfig

--- lenovo/yt2/BoardConfig.mk
+++ lenovo/yt2/BoardConfig.mk
@@ -3,6 +3,8 @@ include build/target/board/generic_x86_64_a/BoardConfig.mk
 TARGET_NO_KERNEL := false
 TARGET_NO_RECOVERY := true
 BOARD_USES_RECOVERY_AS_BOOT := false
+
+# Device bootloader requires ramdisk to be present in the boot partition, so system-as-root isn't possible right now
 BOARD_BUILD_SYSTEM_ROOT_IMAGE := false
 
 BOARD_AVB_ENABLE := false
@@ -28,6 +30,10 @@ BOARD_KERNEL_CMDLINE += printk.devkmsg=on earlyprintk=vga loglevel=8
 BOARD_KERNEL_CMDLINE += androidboot.hardware=yt2 androidboot.spid=xxxx:xxxx:xxxx:xxxx:xxxx:xxxx androidboot.serialno=01234567890123456789 androidboot.selinux=permissive
 BOARD_KERNEL_CMDLINE += firmware_class.path=/vendor/firmware
 BOARD_KERNEL_CMDLINE += vga=current i915.modeset=1 drm.vblankoffdelay=1 acpi_backlight=vendor i915.mipi_panel_id=3
+
+# Uses efilinux bootloader (https://github.com/mfleming/efilinux) along with Intel’s kernelflinger (https://github.com/intel/kernelflinger/)
+# Kernelflinger Android specific code is here(https://github.com/intel/kernelflinger/blob/master/libkernelflinger/android.c) and boot_params data structure used in that file are defined here(https://github.com/mfleming/efilinux/blob/master/loaders/bzimage/bzimage.h)
+# Note: Links don't point to exact source code used in the device, but some of the base code is still the same
 
 BOARD_BOOT_HEADER_VERSION := 0
 BOARD_PAGESIZE := 2048
