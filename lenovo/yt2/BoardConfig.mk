7e8778b889c78355780987f91e480d8575f3d1f0
Sandeep Raghuraman <sandy.8925@gmail.com>
2018-11-22 12:42:12 +0530

    Create fake device tree in initramfs to enable early mount of system and vendor partitions

--- lenovo/yt2/BoardConfig.mk
+++ lenovo/yt2/BoardConfig.mk
@@ -27,7 +27,7 @@ BOARD_CACHEIMAGE_PARTITION_SIZE := 134217728
 BOARD_FLASH_BLOCK_SIZE := 2048
 
 BOARD_KERNEL_CMDLINE += printk.devkmsg=on earlyprintk=vga loglevel=8
-BOARD_KERNEL_CMDLINE += androidboot.hardware=yt2 androidboot.spid=xxxx:xxxx:xxxx:xxxx:xxxx:xxxx androidboot.serialno=01234567890123456789 androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE += androidboot.hardware=yt2 androidboot.spid=xxxx:xxxx:xxxx:xxxx:xxxx:xxxx androidboot.serialno=01234567890123456789 androidboot.selinux=permissive androidboot.android_dt_dir=/$(DEVICE_TREE_DIR)/
 BOARD_KERNEL_CMDLINE += firmware_class.path=/vendor/firmware
 BOARD_KERNEL_CMDLINE += vga=current i915.modeset=1 drm.vblankoffdelay=1 acpi_backlight=vendor i915.mipi_panel_id=3
 
--- lenovo/yt2/aosp_yt2.mk
+++ lenovo/yt2/aosp_yt2.mk
@@ -24,3 +24,25 @@ TARGET_KERNEL_ARCH := x86_64
 BOARD_KERNEL_IMAGE_NAME := bzImage
 
 PRODUCT_COPY_FILES += $(TARGET_KERNEL_SOURCE)/arch/$(TARGET_KERNEL_ARCH)/boot/$(BOARD_KERNEL_IMAGE_NAME):kernel
+
+DEVICE_TREE_DIR := device_tree
+
+PRODUCT_COPY_FILES +=   \
+			device/lenovo/yt2/$(DEVICE_TREE_DIR)/fstab/vendor/mnt_flags:root/$(DEVICE_TREE_DIR)/fstab/vendor/mnt_flags \
+			device/lenovo/yt2/$(DEVICE_TREE_DIR)/fstab/vendor/status:root/$(DEVICE_TREE_DIR)/fstab/vendor/status \
+			device/lenovo/yt2/$(DEVICE_TREE_DIR)/fstab/vendor/type:root/$(DEVICE_TREE_DIR)/fstab/vendor/type \
+			device/lenovo/yt2/$(DEVICE_TREE_DIR)/fstab/vendor/dev:root/$(DEVICE_TREE_DIR)/fstab/vendor/dev \
+			device/lenovo/yt2/$(DEVICE_TREE_DIR)/fstab/vendor/mnt_point:root/$(DEVICE_TREE_DIR)/fstab/vendor/mnt_point \
+			device/lenovo/yt2/$(DEVICE_TREE_DIR)/fstab/vendor/fsmgr_flags:root/$(DEVICE_TREE_DIR)/fstab/vendor/fsmgr_flags \
+			device/lenovo/yt2/$(DEVICE_TREE_DIR)/fstab/system/mnt_flags:root/$(DEVICE_TREE_DIR)/fstab/system/mnt_flags \
+			device/lenovo/yt2/$(DEVICE_TREE_DIR)/fstab/system/status:root/$(DEVICE_TREE_DIR)/fstab/system/status \
+			device/lenovo/yt2/$(DEVICE_TREE_DIR)/fstab/system/type:root/$(DEVICE_TREE_DIR)/fstab/system/type \
+			device/lenovo/yt2/$(DEVICE_TREE_DIR)/fstab/system/dev:root/$(DEVICE_TREE_DIR)/fstab/system/dev \
+			device/lenovo/yt2/$(DEVICE_TREE_DIR)/fstab/system/mnt_point:root/$(DEVICE_TREE_DIR)/fstab/system/mnt_point \
+			device/lenovo/yt2/$(DEVICE_TREE_DIR)/fstab/system/fsmgr_flags:root/$(DEVICE_TREE_DIR)/fstab/system/fsmgr_flags \
+			device/lenovo/yt2/$(DEVICE_TREE_DIR)/fstab/compatible:root/$(DEVICE_TREE_DIR)/fstab/compatible \
+			device/lenovo/yt2/$(DEVICE_TREE_DIR)/compatible:root/$(DEVICE_TREE_DIR)/compatible \
+			device/lenovo/yt2/$(DEVICE_TREE_DIR)/boot_devices:root/$(DEVICE_TREE_DIR)/boot_devices \
+			device/lenovo/yt2/$(DEVICE_TREE_DIR)/vbmeta/parts:root/$(DEVICE_TREE_DIR)/vbmeta/parts \
+			device/lenovo/yt2/$(DEVICE_TREE_DIR)/vbmeta/compatible:root/$(DEVICE_TREE_DIR)/vbmeta/compatible
+#PRODUCT_COPY_FILES += $(call find-copy-subdir-files, *, device/lenovo/yt2/device_tree, root/)
